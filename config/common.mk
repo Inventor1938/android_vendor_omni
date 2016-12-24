MapleAOSP/vendor-maple

Bringup system-squisher for Nougat (2/2)
Other commit is in build
Based on Cl3kener and FusionJacks Lollipop commits

config/common.mk
@@ -106,3 +106,6 @@ export ANDROID_JACK_VM_ARGS += "-Xmx8g"
 
 # Inherit common product build prop overrides
 -include vendor/maple/config/common_versions.mk
+
+# Squisher Location
+SQUISHER_SCRIPT := vendor/maple/tools/squisher

Inventor1938/android_vendor_omni

Signed-off-by: mydongistiny <jaysonedson@gmail.com>

config/common.mk
@@ -74,3 +74,6 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/omni/overlay/common
 ifeq ($(ROM_BUILDTYPE),NIGHTLY)
     WITH_DEXPREOPT := true
 endif
+
+# Squisher Location
+SQUISHER_SCRIPT := vendor/omni/tools/squisher